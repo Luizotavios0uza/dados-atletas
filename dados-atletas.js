22bbba6021ff528d8a0dd1fa475492eca93a14e0
Luizotavios0uza <120200580+Luizotavios0uza@users.noreply.github.com>
2022-12-11 15:44:37 -0300

    Update dados-atletas.js

--- dados-atletas.js
+++ dados-atletas.js
@@ -66,11 +66,7 @@ obtemNomeAtleta(){
         return atleta.calculaMediaValida()
     }
 }
-
-
 const atleta = new Atleta("Cesar Abascal",30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
-
-
 console.log("Nome: " + atleta.obtemNomeAtleta());
 console.log("Idade: " + atleta.obtemIdadeAtleta());
 console.log("Peso: " + atleta.obtemPesoAtleta());
