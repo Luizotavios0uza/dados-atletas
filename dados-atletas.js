b9b568fa97b40e2cc77f0c06ad402bd236efee87
Luizotavios0uza <120200580+Luizotavios0uza@users.noreply.github.com>
2022-12-11 15:51:09 -0300

    Update dados-atletas.js

--- dados-atletas.js
+++ dados-atletas.js
@@ -1,13 +1,13 @@
 class Atleta {
-    constructor(nome, idade, peso, altura, notas) {
-        this.nome = nome;
-        this.idade = idade;
-        this.peso = peso;
-        this.altura = altura;
+    constructor(nome, idade, peso, altura, notas){
+        this.nome = nome
+        this.idade = idade
+        this.peso = peso
+        this.altura = altura
         this.notas = notas
     }
 
-    calculaCategoria() {
+    calculaCategoria(){
         if (this.idade > 8 && this.idade < 12)
             return "Infantil"
         if (this.idade > 11 && this.idade < 14)
@@ -18,22 +18,22 @@ class Atleta {
             return "Adulto"
     }
 
-    calcularIMC() {
+    calcularIMC(){
         return this.peso / (this.altura * this.altura)
     }
 
-    calculaMediaValida() {
+    calculaMediaValida(){
         let notasGerais = this.notas;
         let notasOrganizadas = notasGerais.sort(function comparaNumeros(a, b) {
-            if (a == b) return 0;
-            if (a < b) return -1;
-            if (a > b) return 1;
-        });
+            if (a == b) return 0
+            if (a < b) return -1
+            if (a > b) return 1
+        })
 
 
         let notasComputadas = notasOrganizadas.slice(1, 4)
 
-        let somaNotasComputadas = notasComputadas.reduce(function (total, atual) {
+        let somaNotasComputadas = notasComputadas.reduce(function (total, atual){
             return total + atual
         }, 0)
 
@@ -45,32 +45,32 @@ obtemNomeAtleta(){
         return this.nome
     } 
 
-    obtemIdadeAtleta() {
+    obtemIdadeAtleta(){
         return this.idade;
     } 
-    obtemPesoAtleta() {
+    obtemPesoAtleta(){
         return this.peso;
     } 
-    obtemNotasAtleta() {
+    obtemNotasAtleta(){
         return this.notas;
     } 
 
-    obtemCategoria() {
+    obtemCategoria(){
         return atleta.calculaCategoria()
     } 
 
-    obtemIMC() {
+    obtemIMC(){
         return atleta.calcularIMC()
     } 
-    obtemMediaValida() {
+    obtemMediaValida(){
         return atleta.calculaMediaValida()
     }
 }
-const atleta = new Atleta("Cesar Abascal",30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
-console.log("Nome: " + atleta.obtemNomeAtleta());
-console.log("Idade: " + atleta.obtemIdadeAtleta());
-console.log("Peso: " + atleta.obtemPesoAtleta());
-console.log("Notas: " + atleta.obtemNotasAtleta());
-console.log("Categoria: " + atleta.obtemCategoria());
-console.log("IMC: " + atleta.obtemIMC());
-console.log("Média válida: " + atleta.obtemMediaValida());
+let atleta = new Atleta(30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88])
+console.log("Nome: " + atleta.obtemNomeAtleta())
+console.log("Idade: " + atleta.obtemIdadeAtleta())
+console.log("Peso: " + atleta.obtemPesoAtleta())
+console.log("Notas: " + atleta.obtemNotasAtleta())
+console.log("Categoria: " + atleta.obtemCategoria())
+console.log("IMC: " + atleta.obtemIMC())
+console.log("Média válida: " + atleta.obtemMediaValida())
